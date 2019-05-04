e54395f2027be4c987dc0280dc57c25e901dbfcf
Maggie Leavell <mjleavell@gmail.com>
2019-05-03 20:28:36 -0500

    get services refactored with dapper

--- ClinkedIn2/Data/ServiceRepository.cs
+++ ClinkedIn2/Data/ServiceRepository.cs
@@ -2,6 +2,8 @@
 using ClinkedIn2.Models;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
 
 namespace ClinkedIn2.Data
 {
@@ -43,31 +45,39 @@ namespace ClinkedIn2.Data
             throw new Exception("No Service found");
         }
 
-        public List<Services> GetAllServices()
+        public IEnumerable<Services> GetAllServices()
         {
-            var services = new List<Services>();
-            var connection = new SqlConnection(ConnectionString);
-            connection.Open();
-
-            var getAllUsersCommand = connection.CreateCommand();
-            getAllUsersCommand.CommandText = "select * from services";
-
-            var reader = getAllUsersCommand.ExecuteReader(); // Excecute the reader! // if you don't care about the result and just want to know how many things were affected, use the ExecuteNonQuery
-                                                             // ExecuteScalar for top left value - 1 column / 1 row
-            while (reader.Read())
+            using (var db = new SqlConnection(ConnectionString))
             {
-                var id = (int)reader["Id"]; //(int) is there to turn it into an int
-                var name = reader["name"].ToString();
-                var description = reader["description"].ToString();
-                var price = (decimal)reader["price"];
-                var newService = new Services(id, name, description, price);
+                var getQuery = "SELECT * FROM Services";
 
-                services.Add(newService);
+                var services = db.Query<Services>(getQuery).ToList();
+
+                return services;
             }
+            //var services = new List<Services>();
+            //var connection = new SqlConnection(ConnectionString);
+            //connection.Open();
 
-            connection.Close(); // Close it down!
+            //var getAllUsersCommand = connection.CreateCommand();
+            //getAllUsersCommand.CommandText = "select * from services";
 
-            return services;
+            //var reader = getAllUsersCommand.ExecuteReader(); // Excecute the reader! // if you don't care about the result and just want to know how many things were affected, use the ExecuteNonQuery
+            //                                                 // ExecuteScalar for top left value - 1 column / 1 row
+            //while (reader.Read())
+            //{
+            //    var id = (int)reader["Id"]; //(int) is there to turn it into an int
+            //    var name = reader["name"].ToString();
+            //    var description = reader["description"].ToString();
+            //    var price = (decimal)reader["price"];
+            //    var newService = new Services(id, name, description, price);
+
+            //    services.Add(newService);
+            //}
+
+            //connection.Close(); // Close it down!
+
+            //return services;
         }
     }
 }
